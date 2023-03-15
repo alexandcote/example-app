cd99e751b07dd6388ec55c6a85dfacdb7cac311b
Alexandre Côté <alex.cote@shopify.com>
2023-03-15 18:06:44 -0400

    Fixing spin CDN

--- config/server.ts
+++ config/server.ts
@@ -3,5 +3,5 @@ export const port = parseInt(process.env.PORT!, 10) || 3000;
 
 const isSpin = Boolean(process.env.SPIN_INSTANCE_FQDN);
 export const assetsUrl = isSpin
-  ? `https://example-app-cdn.${process.env.SPIN_INSTANCE_FQDN}/webpack/assets/`
+  ? `https://${process.env.SPIN_INSTANCE_FQDN}/webpack/assets/`
   : `http://localhost:8080/webpack/assets/`;
